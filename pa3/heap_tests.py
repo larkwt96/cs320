c4e02419880a92a1f8e6b5bb1415a87f52f80bba
Lucas Wilson <lkwilson96@gmail.com>
2018-03-27 13:04:24 -0600

    Add pa3

---
+++ pa3/heap_tests.py
@@ -0,0 +1,151 @@
+from argparse import ArgumentParser
+import heap
+
+
+def test_heapify():
+    print("#test_heapify")
+    A = [1]
+    heap.heapify(A, 0)
+    print("Heapify length 1:", A == [1])
+
+    A = [2, 1]
+    heap.heapify(A, 0)
+    print("Heapify two out of order:", A == [1, 2])
+
+    A = [1, 2]
+    heap.heapify(A, 0)
+    print("Heapify two in order:", A == [1, 2])
+
+    A = [1, 4, 3, 5, 2]
+    heap.heapify(A, 1)
+    print("Heapify five needing swap:", A == [1, 2, 3, 5, 4])
+
+    A = [1, 8, 3, 9, 2, 10, 11, 12, 13, 7, 6]
+    heap.heapify(A, 1)
+    print("Heapify five needing recursive swap:", A == [1, 2, 3, 9, 6, 10, 11, 12, 13, 7, 8])
+
+    A = [1, 4, 3, 5, 2]
+    heap.heapify(A, 1, 4)
+    print("Heapify five with limit number:", A == [1, 4, 3, 5, 2])
+
+
+def is_min_heap(A):
+    def parent(i):
+        return (i - 1) // 2
+    return all(A[parent(x)] <= A[x] for x in range(1, len(A)))
+
+
+def shuffled_list(length, seed):
+    A = list(range(10, length + 10))
+    import random
+    r = random.Random(seed) # pseudo random, so it is repeatable
+    r.shuffle(A)
+    return A
+
+
+def test_build_heap():
+    print("#test_build_heap")
+    for x in range(5, 41, 5):
+        A = shuffled_list(x, x)
+        heap.buildHeap(A)
+        print("buildHeap(shuffled_list({}, {})) is a min heap: ".format(x, x), is_min_heap(A))
+
+
+def test_insert_extract():
+    print("#test_insert_extract")
+    for round in range(3):
+        order = shuffled_list(50, round)
+        A = []
+        for x in range(20):
+            heap.heapInsert(A, order.pop())
+            heap.heapInsert(A, order.pop())
+            has_heap_property = is_min_heap(A)
+            min_elem = min(A)
+            extracted = heap.heapExtractMin(A)
+            print("Correctly extracted min: {}, maintained heap property: {}".format(
+                min_elem == extracted, has_heap_property and is_min_heap(A)))
+
+
+def is_sorted(A):
+    for i in range(0, len(A) - 1):
+        if not A[i] >= A[i+1]:
+            return False
+    return True
+
+
+def test_heap_sort():
+    print("#test_heap_sort:")
+    A = shuffled_list(1000, 0)
+    heap.heapSort(A)
+    print("Correctly sorted 1000 shuffled: {}".format(is_sorted(A)))
+
+
+def within_20_percent(expected, actual):
+    margin = 0.2 * expected
+    low = expected - margin
+    hi = expected + margin
+    return low <= actual <= hi
+
+    
+def report_counts_on_basic_ops(A, expected_build_count, expected_extract_count, expected_insert_count):
+    print("Counts for list of len: {}".format(len(A)))
+    heap.reset_counts()
+    heap.buildHeap(A)
+    bh = heap.current_counts()['heapify_call_count']
+    print("buildHeap heapify calls within 20% of expected number of calls: {}".format(within_20_percent(expected_build_count, bh)))
+
+    heap.reset_counts()
+    m = heap.heapExtractMin(A)
+    ex = heap.current_counts()['heapify_call_count']
+    print("heapExtractMin heapify calls within 20% of expected number of calls: {}".format(within_20_percent(expected_extract_count, ex)))
+
+    heap.reset_counts()
+    heap.heapInsert(A, m)
+    ins = heap.current_counts()['swap_count']
+    print("heapInsert swap calls within 20% of expected number of calls: {}".format(within_20_percent(expected_insert_count, ins)))
+
+
+def test_counts():
+    print("#test_counts:")
+    
+    A = shuffled_list(400, 0)
+    report_counts_on_basic_ops(A, 487, 8, 8)
+    
+    A = shuffled_list(10000, 0)
+    report_counts_on_basic_ops(A, 12420, 14, 13)
+
+    A = shuffled_list(100000, 0)
+    report_counts_on_basic_ops(A, 124571, 17, 16)
+
+            
+def test_all():
+    test_heapify()
+    test_build_heap()
+    test_insert_extract()
+    test_heap_sort()
+    test_counts()
+
+
+def test_by_number(test_number):
+    if test_number == 1:
+        test_heapify()
+    elif test_number == 2:
+        test_build_heap()
+    elif test_number == 3:
+        test_insert_extract()
+    elif test_number == 4:
+        test_heap_sort()
+    else:
+        test_counts()
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument("--test", dest="test_number",
+                        type=int, help="The number of the test to run [1-5]")
+    args = parser.parse_args()
+
+    if args.test_number:
+        test_by_number(args.test_number)
+    else:
+        test_all()
